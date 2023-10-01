21ecde46853b04c3d9236fa3c5d12f106ebab4c3
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-10-01 14:44:52 +0300

    Вложенность стилей

--- vue/src/components/BookForm.vue
+++ vue/src/components/BookForm.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="book-input">
-    <el-input v-model="form.author" placeholder="Автор" />
-    <el-input v-model="form.initials" placeholder="Инициалы автора" />
-    <el-input v-model="form.title" placeholder="Заглавие" />
-    <el-input v-model="form.place" placeholder="Место издания" />
-    <el-input v-model="form.publisher" placeholder="Издательство" />
-    <el-input v-model="form.year" placeholder="Год издания" />
-    <el-input v-model="form.pages" placeholder="Количество страниц" />
-    <el-input v-model="form.additionals" placeholder="Дополнительные сведения" />
-    <el-input v-model="form.isbn" placeholder="ISBN" />
+  <div class="book-form">
+    <div class="book-form__input"> 
+      <el-input v-model="form.author" placeholder="Автор" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.initials" placeholder="Инициалы автора" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.title" placeholder="Заглавие" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.place" placeholder="Место издания" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.publisher" placeholder="Издательство" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.year" placeholder="Год издания" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.pages" placeholder="Количество страниц" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.additionals" placeholder="Дополнительные сведения" />
+    </div>
+    <div class="book-form__input"> 
+      <el-input v-model="form.isbn" placeholder="ISBN" />
+    </div>
     <el-button type="primary" @click="() => save()">Сохранить</el-button>
   </div>
-
 </template>
 
 <script>
   export default {
-    name: 'BookInputComponent',
+    name: 'BookForm',
     data () {
       return {
         form: {
@@ -33,7 +50,7 @@
       }
     },
     methods: {
-      save() {
+      save () {
         console.log(this.form)
       }
     }
@@ -41,12 +58,13 @@
 </script>
 
 <style scoped lang="less">
-  .book-input {
+  .book-form {
     padding: 3px;
     margin-bottom: 5px;
+    
+    &__input {
+      margin-bottom: 5px;
+    }
   }
 
-  .book-input .el-input {
-    margin-bottom: 5px;
-  }
 </style>
